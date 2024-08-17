13efb46483e00b27a8ff280b1080910ad0d961cc
kuro Indra <143628023+J02Jesus@users.noreply.github.com>
2024-08-16 23:58:00 -0300

    Prova de 2014.2 I questao 2.c

--- Prova de 2014.2 I questao 2.c
+++ Prova de 2014.2 I questao 2.c
@@ -1,4 +1,9 @@
-#include <stdio.h>
+/**Escreva um programa em C, que receba três números representando uma coordenada
+geográfica expressa em graus, minutos e segundos, calcule e imprima este valor em
+graus decimais. Para tanto, considere que 60 minutos equivalem a um grau e 60
+segundos equivalem a um minuto. Assim, por exemplo, 37º39’20’’ (37 graus, 39 minutos
+e 20 segundos) equivalem, aproximadamente, a 37.65556 graus decimais.
+**/
 
 int main() {
   float grau, seg, min;
